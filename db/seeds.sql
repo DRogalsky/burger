3e2ef3b40ef82399f659893cf9c3c88d4d7e0e98
dnl092 <drogalsky14@my.whitworth.edu>
2019-07-12 20:26:53 -0700

    just added another burger

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Hamburger');
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon burger');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
